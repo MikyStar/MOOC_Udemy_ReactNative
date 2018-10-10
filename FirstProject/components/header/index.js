e8ecd453c4050b9fefc89ffce51582d0d20fb3df
Mike AUBENAS <mike.aubenas@gmail.com>
2018-10-10 14:38:31 +0200

    A cleaner way to send props (props = content to component)

--- FirstProject/components/header/index.js
+++ FirstProject/components/header/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { Text } from 'react-native'
 
-const Header = (props) => // Be carefull, if I'm not using the render function to use JSX, then it's not {} but it is ()
+const Header = ( {content} ) => // Be carefull, if I'm not using the render function to use JSX, then it's not {} but it is ()
 (
-	<Text>{props.content}</Text>
+	<Text>{content}</Text>
 );
 
 export default Header;

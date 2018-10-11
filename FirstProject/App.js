66379be17f86425bfc5557455967dd91d413cc21
Mike AUBENAS <mike.aubenas@gmail.com>
2018-10-11 11:50:22 +0200

    Revert "Removed the button because it will not be usefull for the app"
    This reverts commit 714a5cfee42f2bc80a3b0a83f903cb9d53bb7780.

--- FirstProject/App.js
+++ FirstProject/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View } from 'react-native'
+import { Button as ButtonRNE} from 'react-native-elements' // The alias here is not necessary, but if someday there's the same name in my imports, I can distinguish with that
 
 import Header from './components/header'
 
@@ -10,6 +11,13 @@ export default class App extends React.Component
 		return (
 			<View>
 				<Header content="Liste de tâches " content2="en props !" />
+
+				<ButtonRNE
+					large
+					iconRight
+					icon={ {name : 'code'} }
+					title="I'm a button"
+				/>
 			</View>
 		);
 	}

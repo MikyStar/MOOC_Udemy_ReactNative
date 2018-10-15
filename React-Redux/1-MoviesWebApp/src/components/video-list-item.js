8c158fb0ad1dbf5e646130d564ac2b54a2e62c33
Mike AUBENAS <mike.aubenas@gmail.com>
2018-10-15 15:51:13 +0200

    Add movies images to movie item component

--- React-Redux/1-MoviesWebApp/src/components/video-list-item.js
+++ React-Redux/1-MoviesWebApp/src/components/video-list-item.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import datas from '../../sensibleInformations'
 
 const VideoListItem = ( { movie } ) => // { movie } = let movie = props.movie
 {
-	return <li>Un film recommandé : { movie.title } </li>
+	return (
+		<li>
+			<img
+				src={ `${ datas.IMAGE_BASE_URL }${ movie.poster_path }` }
+				height="120px"
+				width="100px"
+			/>
+			<h3>{movie.title}</h3>
+		</li>
+	);
 }
 
 // It's better to directly explose the props element with the {} writing because props are read only, so if someday I want to change the value of a props, it's the only way to de it

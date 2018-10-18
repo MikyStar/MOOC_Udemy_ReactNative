1cd242315d14852f7d1459f690f05702bd0b8724
Mike AUBENAS <mike.aubenas@gmail.com>
2018-10-18 16:11:20 +0200

    WIP on mortality charts : they don't work but I had a CORS problem ...
    .. which is now resolved I don't know why

--- React-Redux/3-Mortality/src/components/mortality-list-item.js
+++ React-Redux/3-Mortality/src/components/mortality-list-item.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import ReactChartkick, { ColumnChart } from 'react-chartkick'
+import Chart from 'chart.js'
 
 import Flag from './flag';
+
+ReactChartkick.addAdapter( Chart );
 
 const MortalityListItem = () =>
 {
@@ -10,6 +14,10 @@ const MortalityListItem = () =>
 			<Flag
 				country={ 'France' }
 				className={ 'flag_medium' }
+			/>
+
+			<ColumnChart
+				data={ [ [12, 13], [17, 10], [12, 16], [2, 10] ] }
 			/>
 
 		</div>

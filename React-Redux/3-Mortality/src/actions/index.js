de0c2d6662e2bb7b36442e68f5093e2c8d1ef1fd
Mike AUBENAS <mike.aubenas@gmail.com>
2018-10-18 10:45:02 +0200

    Created a Redux-Thunk middleware to handle asynchronous actions

---
+++ React-Redux/3-Mortality/src/actions/index.js
@@ -0,0 +1,35 @@
+import axios from 'axios'
+
+import urlManager from '../assets/urlManager'
+
+export const GET_COUNTRIES = "GET_COUNTRIES"
+export const ERROR_GET_COUNTRIES = "ERROR_GET_COUNTRIES"
+
+/**
+ * Redux is not able to handle asynchronous.
+ * Thanks to Redux-Thunk we can make it do that.
+ * So basically, here we have created a middleware between the action and all reducers. This middleware says you know what don't give me an action, just give me the function and giving that we are doing something asynchronous, whenever the action is done, I will send the proper action to all reducers
+ */
+export function getCountries()
+{
+	return function ( dispatch )
+	{
+		axios.get( urlManager.POPULATION_COUNTRIES ).then(
+		( response ) =>
+		{
+			dispatch(
+			{
+				type : GET_COUNTRIES,
+				payload : response.data.countries
+			});
+		}
+		).catch( ( error ) =>
+		{
+			dispatch(
+			{
+				type : ERROR_GET_COUNTRIES,
+				error : error.response.data.detail
+			});
+		})
+	}
+}
---
+++ React-Redux/3-Mortality/src/assets/urlManager.js
@@ -0,0 +1,4 @@
+module.exports =
+{
+	POPULATION_COUNTRIES : "http://api.population.io:80/1.0/countries"
+}

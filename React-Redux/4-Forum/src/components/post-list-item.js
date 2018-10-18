24ba84883bf45fd4975c012a31b1118bca7108e6
Mike AUBENAS <mike.aubenas@gmail.com>
2018-10-18 22:53:27 +0200

    Created the very basis of a skeleton

---
+++ React-Redux/4-Forum/src/components/post-list-item.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const PostListItem = () =>
+{
+	return (
+		<div>
+
+			<p>Element de la liste</p>
+
+		</div>
+	)
+}
+
+export default PostListItem;
---
+++ React-Redux/4-Forum/src/containers/post.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+import PostContent from '../components/post-content';
+
+class Post extends Component
+{
+	render()
+	{
+		return (
+			<div>
+
+				<PostContent />
+
+			</div>
+		)
+	}
+}
+
+export default Post;

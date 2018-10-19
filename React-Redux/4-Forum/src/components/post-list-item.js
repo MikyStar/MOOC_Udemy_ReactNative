075cbfb79a14695508e0242589bcbfb43f2732bf
Mike AUBENAS <mike.aubenas@gmail.com>
2018-10-19 11:14:31 +0200

    Created links on list articles to go to them thanks to react-router

--- React-Redux/4-Forum/src/components/post-list-item.js
+++ React-Redux/4-Forum/src/components/post-list-item.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 const PostListItem = ( props ) =>
 {
@@ -7,7 +8,9 @@ const PostListItem = ( props ) =>
 	return (
 		<tr>
 
-			<td>{ post.title }</td>
+			<td>
+				<Link to={`post/${post.id}`}>{post.title}</Link>
+			</td>
 			<td>
 				<button className='btn btn-danger '>Supprimer</button>
 			</td>

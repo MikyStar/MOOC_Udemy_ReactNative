7ef018e05d77a0e5a90b4cab129c740c5a799b5a
Mike AUBENAS <mike.aubenas@gmail.com>
2018-10-24 17:14:14 +0200

    Removed useless things in Main.jsx + some-text.jsx

--- React-Native/albums/src/Main.jsx
+++ React-Native/albums/src/Main.jsx
@@ -1,19 +1,17 @@
 import React, { Component } from 'react';
 import { Platform, Text, View } from 'react-native';
 
-import SomeText from './components/some-text';
 import Header from './components/header';
 
-const instructions = Platform.select( {
+const instructions = Platform.select(
+{
 	ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
 	android:
 		'Double tap R on your keyboard to reload,\n' +
 		'Shake or press menu button for dev menu',
-} );
+});
 
-type Props = {};
-
-export default class Main extends Component<Props>
+export default class Main extends Component
 {
 	render()
 	{
--- React-Native/albums/src/components/some-text.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Text, AppRegistry } from 'react-native';
-
-const SomeText = () =>
-(
-	<Text>Some Text</Text>
-);
-
-export default SomeText;

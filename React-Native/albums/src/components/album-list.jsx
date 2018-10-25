6a2f57e6c6b7125f84e49f0e4bcff6b377b70d77
Mike AUBENAS <mike.aubenas@gmail.com>
2018-10-25 10:15:30 +0200

    Puting it to the state (without passing by constructor, it works)

--- React-Native/albums/src/components/album-list.jsx
+++ React-Native/albums/src/components/album-list.jsx
@@ -3,17 +3,21 @@ import { View, Text } from 'react-native';
 
 export default class AlbumList extends Component
 {
+	state = { albums : [] };
+
 	componentWillMount()
 	{
 		fetch( 'https://rallycoding.herokuapp.com/api/music_albums' ).then(
 			( response ) => response.json()
 		).then(
-			( responseData ) => console.log( responseData )
+			( responseData ) => this.setState( { albums : responseData } )
 		);
 	}
 
 	render()
 	{
+		console.log(this.state);
+
 		return (
 			<View>
 				<Text>AlbumList !!</Text>

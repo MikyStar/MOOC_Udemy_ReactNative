11c6e14de41f97eb8b9e7165c1734585cccb6e7d
Mike AUBENAS <mike.aubenas@gmail.com>
2018-10-25 15:14:29 +0200

    WIP : image rendered

--- React-Native/albums/src/components/album-detail.jsx
+++ React-Native/albums/src/components/album-detail.jsx
@@ -1,24 +1,33 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 
 import Card from './card'
 import CardSection from './cars-section';
 
-export default AlbumDetail = ( props ) =>
+export default AlbumDetail = ( { album } ) =>
 {
+	const { title, thumbnail_image, artist } = album;
+
 	return (
 		<Card>
 			<CardSection>
+
 				<View>
+
+					<Image
+						source={ { uri : thumbnail_image } }
+						style={ styles.thumbnailStyle }
+					/>
 
 				</View>
 
 				<View style={ styles.headerContentStyle }>
 
-					<Text>{ props.album.title }</Text>
-					<Text>{ props.album.artist }</Text>
+					<Text>{ title }</Text>
+					<Text>{ artist }</Text>
 
 				</View>
+
 			</CardSection>
 		</Card>
 	);
@@ -30,5 +39,10 @@ const styles =
 	{
 		flexDirecton : 'column',
 		justifyContent : 'space-around'
+	},
+	thumbnailStyle : // ! An Image tag cannot be rendered if theres no hight and width
+	{
+		height : 50,
+		width : 50
 	}
 }

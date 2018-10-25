c7ded5d7ebb09db457355f979fca6caf773e4680
Mike AUBENAS <mike.aubenas@gmail.com>
2018-10-25 16:06:14 +0200

    Displaying covers + styling again

--- React-Native/albums/src/components/album-detail.jsx
+++ React-Native/albums/src/components/album-detail.jsx
@@ -6,13 +6,13 @@ import CardSection from './cars-section';
 
 export default AlbumDetail = ( { album } ) =>
 {
-	const { title, thumbnail_image, artist } = album;
+	const { title, thumbnail_image, artist, image } = album;
 
 	return (
 		<Card>
 			<CardSection>
 
-				<View>
+				<View style={ styles.thumbnailContainerStyle }>
 
 					<Image
 						source={ { uri : thumbnail_image } }
@@ -23,10 +23,19 @@ export default AlbumDetail = ( { album } ) =>
 
 				<View style={ styles.headerContentStyle }>
 
-					<Text>{ title }</Text>
+					<Text style={ styles.headerTextStyle }>{ title }</Text>
 					<Text>{ artist }</Text>
 
 				</View>
+
+			</CardSection>
+
+			<CardSection>
+
+				<Image
+					source={ { uri : image } }
+					style={ styles.imageStyle }
+				/>
 
 			</CardSection>
 		</Card>
@@ -40,9 +49,27 @@ const styles =
 		flexDirecton : 'column',
 		justifyContent : 'space-around'
 	},
+	headerTextStyle :
+	{
+		fontSize : 18,
+	},
 	thumbnailStyle : // ! An Image tag cannot be rendered if theres no hight and width
 	{
 		height : 50,
 		width : 50
+	},
+	thumbnailContainerStyle :
+	{
+		justifyContent : 'center',
+		alignItems : 'center',
+		marginLeft : 10,
+		marginRight : 10,
+	},
+	imageStyle :
+	{
+		// ! Don't ask any question, this combinaison is magic and makes it take the full size possible of where the Image is located
+		height : 300,
+		flex : 1,
+		width : null
 	}
 }

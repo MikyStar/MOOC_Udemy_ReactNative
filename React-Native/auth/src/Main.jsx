3041eb9bcb7cd95f2d00d4f9fe500b3743e064d1
Mike AUBENAS <mike.aubenas@gmail.com>
2018-10-26 15:43:20 +0200

    Now able to sign out

--- React-Native/auth/src/Main.jsx
+++ React-Native/auth/src/Main.jsx
@@ -3,14 +3,48 @@ import { View } from 'react-native';
 import Firebase from 'firebase';
 
 import sensibleInformations from './assets/sensibleInformations'
-import { Header } from './components/common';
+import { Header, Button, CardSection, Spinner } from './components/common';
 import LoginForm from './components/login-form';
 
 export default class Main extends Component
 {
+	state =
+	{
+		loggedIn : null
+	};
+
 	componentWillMount()
 	{
 		Firebase.initializeApp( sensibleInformations.firebase )
+
+		Firebase.auth().onAuthStateChanged( user =>
+		{
+			if( user )
+				this.setState( { loggedIn : true } );
+			else
+				this.setState( { loggedIn: false } )
+		});
+	}
+
+	renderContent()
+	{
+		switch( this.state.loggedIn )
+		{
+			case true :
+				return (
+					<CardSection>
+						<Button
+						whenPressed={ () => Firebase.auth().signOut() }
+						>
+							Log out
+						</Button>
+					</CardSection>
+				);
+			case false :
+				return <LoginForm />
+			default :
+					return <Spinner size='large' />
+		}
 	}
 
 	render()
@@ -18,7 +52,9 @@ export default class Main extends Component
 		return (
 			<View>
 				<Header text={'Authentification'} />
-				<LoginForm />
+
+				{ this.renderContent() }
+
 			</View>
 		);
 	}

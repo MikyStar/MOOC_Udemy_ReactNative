a92c282dec34cfa4040b787cb3511e9de25b6ec4
Mike AUBENAS <mike.aubenas@gmail.com>
2018-10-26 17:13:40 +0200

    Created the very basis of Redux on a RN app

--- React-Native/TechStackRedux/src/Main.jsx
+++ React-Native/TechStackRedux/src/Main.jsx
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'react-redux'
+
+import reducers from './reducers'
 
 export default class Main extends Component
 {
 	render()
 	{
 		return(
-			<View>
-				<Text>Test</Text>
-			</View>
+			<Provider store={ createStore( reducers ) } >
+
+				<View>
+					<Text>Test</Text>
+				</View>
+
+			</Provider>
 		);
 	}
 }
+
+/*
+! The store contain the different states of the application
+! The Provider is what makes the link between the store and the React application
+*/

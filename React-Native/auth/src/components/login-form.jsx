da430513fa6ae889805fe305ac3bc9b2539fe4f5
Mike AUBENAS <mike.aubenas@gmail.com>
2018-10-26 11:56:19 +0200

    WIP text input

--- React-Native/auth/src/components/login-form.jsx
+++ React-Native/auth/src/components/login-form.jsx
@@ -1,20 +1,35 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { TextInput } from 'react-native';
 
 import { Card, CardSection, Button} from './common';
 
 export default class LoginForm extends Component
 {
+	state = { text : '' };
+
 	render()
 	{
 		return(
 			<Card>
-				<CardSection />
-				
+				<CardSection>
+
+					{/* Just like the Image tag, TextInput by default has no height and width
+						We also have to pass by the value attribute because RN is a bit strange with TextInpit
+						and the TextInput never know what value it has*/}
+					<TextInput
+						style={ { height : 20, width : 100 } }
+						value={ this.state.text }
+						onChangeText={ text => this.setState( { text }) }
+					/>
+
+				</CardSection>
+
 				<CardSection />
 
 				<CardSection>
+
 					<Button>Login</Button>
+
 				</CardSection>
 			</Card>
 		);

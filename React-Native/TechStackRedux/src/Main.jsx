7465855cae6763cb0656861e13002e92a3f3ac5a
Mike AUBENAS <mike.aubenas@gmail.com>
2018-10-28 18:01:09 +0100

    Imported common components + Fix Type Error react.createStore

--- React-Native/TechStackRedux/src/Main.jsx
+++ React-Native/TechStackRedux/src/Main.jsx
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
-import { createStore } from 'react-redux'
+import { createStore } from 'redux'
 
 import reducers from './reducers'
+import { Header } from './components/common';
 
 export default class Main extends Component
 {
@@ -11,11 +12,13 @@ export default class Main extends Component
 	{
 		return(
 			<Provider store={ createStore( reducers ) } >
-
 				<View>
-					<Text>Test</Text>
-				</View>
 
+					<Header text='Tech Stack' />
+
+					<Text>Test</Text>
+
+				</View>
 			</Provider>
 		);
 	}
@@ -24,4 +27,5 @@ export default class Main extends Component
 /*
 ! The store contain the different states of the application
 ! The Provider is what makes the link between the store and the React application
+! The Provider can only have one child, so one big View
 */
---
+++ React-Native/TechStackRedux/src/components/common/index.js
@@ -0,0 +1,6 @@
+export * from './button'
+export * from './card-section'
+export * from './header'
+export * from './card'
+export * from './input'
+export * from './spinner'

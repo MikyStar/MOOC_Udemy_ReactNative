8622284637a7529b0cfb2c2abf6f00f7286d101b
Mike AUBENAS <mike.aubenas@gmail.com>
2018-10-28 19:10:35 +0100

    UI back : display basic ugly list of libraries

--- React-Native/TechStackRedux/src/components/library-list.jsx
+++ React-Native/TechStackRedux/src/components/library-list.jsx
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
+import { FlatList } from 'react-native';
 import { connect } from 'react-redux';
+
+import ListItem from './list-item';
 
 class LibraryList extends Component
 {
+	renderItem( library )
+	{
+		return <ListItem library={ library } />
+	}
+
 	render()
 	{
-		console.log('props', this.props);
-		return;
+		/**
+		 * @description Much better than the map function in performance for lists that has all his elements can't be shown at the same time on the screen -> Same system than RecyclerView in Android
+		 */
+		return(
+			<FlatList
+				data={ this.props.libraries }
+				renderItem={ this.renderItem }
+				keyExtractor={ ( library ) => library.id }
+			/>
+		);
 	}
 }
 
 /**
- *	Purpose -> Take the global state object (in the ReduxStore), and get some properties out off it to put them on props of the current component
- * The thing returned by that function is an object that is going to be accessible inside the props inside this component
+ *	@description Take the global state object (in the ReduxStore), and get some properties out off it to put them on props of the current component
+ * @returns an object that is going to be accessible inside the props inside this component
  */
 const mapStateToProps = state =>
 {

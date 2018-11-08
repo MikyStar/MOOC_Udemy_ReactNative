c80c6e9e760dd615afad70a903511f57ee963aa9
Mike AUBENAS <mike.aubenas@gmail.com>
2018-11-08 15:15:30 +0100

    PROBLEM : Edit form not populated

--- React-Native/Manager/src/components/employee-edit.jsx
+++ React-Native/Manager/src/components/employee-edit.jsx
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import _ from 'lodash'
 
 import { Card, CardSection, Button } from './common';
 import EmployeeForm from './employee-form';
+import { employeeUpdate } from '../actions/employee-actions';
 
 class EmployeeEdit extends Component
 {
+	componentWillMount()
+	{
+		_.each( this.props.employee, ( value, key ) =>
+		{
+			this.props.employeeUpdate( { key, value } );
+		})
+	}
+
+	onButtonPressed()
+	{
+		const { name, phone, shift } = this.props;
+
+		console.log( name, phone, shift );
+	}
+
 	render()
 	{
 		return	(
@@ -14,7 +31,7 @@ class EmployeeEdit extends Component
 						<EmployeeForm />
 
 						<CardSection>
-							<Button>Save changes</Button>
+							<Button whenPressed={ this.onButtonPressed.bind( this ) }>Save changes</Button>
 						</CardSection>
 
 					</Card>
@@ -22,5 +39,11 @@ class EmployeeEdit extends Component
 	}
 }
 
+const mapStateToProps = state =>
+{
+	const { name, phone, shift } = state.employeeForm;
 
-export default connect()( EmployeeEdit );
+	return { name, phone, shift };
+};
+
+export default connect( mapStateToProps, { employeeUpdate })( EmployeeEdit );

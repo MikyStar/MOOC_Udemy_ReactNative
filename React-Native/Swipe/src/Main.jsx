bbf8f2729a4031ae86b054f33a44da47d516a453
Mike AUBENAS <mike.aubenas@gmail.com>
2018-11-09 09:51:38 +0100

    Animation : The ball is moving from top left to bottom/middle right

--- React-Native/Swipe/src/Main.jsx
+++ React-Native/Swipe/src/Main.jsx
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 
+import Ball from './Ball';
+
 class Main extends Component
 {
 	render()
 	{
 		return	(
-					<View>
-						<Text>Hello World !</Text>
+					<View style={ styles.container }>
+						<Ball />
 					</View>
 				);
+	}
+}
+
+const styles =
+{
+	container :
+	{
+		flex : 1,
+		backgroundColor : '#fff'
 	}
 }
 

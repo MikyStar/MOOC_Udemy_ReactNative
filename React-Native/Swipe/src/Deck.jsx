b45176d0a1025b2a20a52c0cbef69e4f216fc25a
Mike AUBENAS <mike.aubenas@gmail.com>
2018-11-14 16:41:27 +0100

    Refactor so now can smoothly eject right or left

--- React-Native/Swipe/src/Deck.jsx
+++ React-Native/Swipe/src/Deck.jsx
@@ -45,11 +45,11 @@ class Deck extends Component
 			{
 				if( gesture.dx > SWIPE_THRESHOLD )
 				{
-					this.forceSwipeRight();
+					this.forceSwipe( 'right' );
 				}
 				else if( gesture.dx < -SWIPE_THRESHOLD )
 				{
-					console.log('swipe left');
+					this.forceSwipe( 'left' );
 				}
 				else
 				{
@@ -61,11 +61,13 @@ class Deck extends Component
 		this.state = { panResponder, position }
 	}
 
-	forceSwipeRight()
+	forceSwipe( direction )
 	{
+		const x = direction === 'right' ? SCREEN_WIDTH : -SCREEN_WIDTH;
+
 		Animated.timing( this.state.position,
 		{
-			toValue : { x : SCREEN_WIDTH, y : 0 },
+			toValue : { x, y : 0 },
 			duration : SWIPE_OUT_DURATION
 		}).start();
 	}

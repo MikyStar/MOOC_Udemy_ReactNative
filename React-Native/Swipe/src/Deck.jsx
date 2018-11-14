37401212f9d0a8b2933e2f7c40b312e16ace5ca7
Mike AUBENAS <mike.aubenas@gmail.com>
2018-11-14 16:35:03 +0100

    Smoothly eject a card to right to finish user gesture

--- React-Native/Swipe/src/Deck.jsx
+++ React-Native/Swipe/src/Deck.jsx
@@ -3,6 +3,7 @@ import { View, PanResponder, Animated, Dimensions } from 'react-native';
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
 const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
+const SWIPE_OUT_DURATION = 250;
 
 class Deck extends Component
 {
@@ -44,7 +45,7 @@ class Deck extends Component
 			{
 				if( gesture.dx > SWIPE_THRESHOLD )
 				{
-					console.log('swipe right')
+					this.forceSwipeRight();
 				}
 				else if( gesture.dx < -SWIPE_THRESHOLD )
 				{
@@ -58,6 +59,15 @@ class Deck extends Component
 		});
 
 		this.state = { panResponder, position }
+	}
+
+	forceSwipeRight()
+	{
+		Animated.timing( this.state.position,
+		{
+			toValue : { x : SCREEN_WIDTH, y : 0 },
+			duration : SWIPE_OUT_DURATION
+		}).start();
 	}
 
 	resetPosition()

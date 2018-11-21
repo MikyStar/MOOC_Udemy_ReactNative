2eb447e8d13a4e0bf8486049bff117468ff6c637
Mike AUBENAS <mike.aubenas@gmail.com>
2018-11-21 14:11:12 +0100

    WIP on connection to DB

--- Firebase/OPT/functions/index.js
+++ Firebase/OPT/functions/index.js
@@ -1,14 +1,19 @@
+const admin = require('firebase-admin');
 const { onRequest } = require("firebase-functions").https;
+
+const createUser = require('./create-user');
+const serviceAccount = require('./assets/credentials.json');
+
+/**
+ * Allows us to access Firebase things like database for instance
+ */
+admin.initializeApp(
+{
+	credential: admin.credential.cert( serviceAccount ),
+	databaseURL: "https://mooc-reactnative-otp.firebaseio.com"
+} );
 
 module.exports =
 {
-	helloWorld : onRequest( ( resuest, response) =>
-	{
-		response.send( 'Hello from Firebase !' );
-	}),
-
-	goodBye : onRequest( ( request, response ) =>
-	{
-		response.send( ' Seeya !' );
-	})
+	createUser : onRequest( createUser )
 }

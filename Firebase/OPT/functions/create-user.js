5d0ae19cd8b29806c8e87a88a3ed49e57422ec39
Mike AUBENAS <mike.aubenas@gmail.com>
2018-11-21 14:58:49 +0100

    Create a unique user in DataBase

--- Firebase/OPT/functions/create-user.js
+++ Firebase/OPT/functions/create-user.js
@@ -1,4 +1,13 @@
+const admin = require( 'firebase-admin' );
+
 module.exports = ( request, response ) =>
 {
-	response.send( request.body );
+	if( !request.body.phone )
+		return response.status( 422 ).send( { error : 'Bad Input' });
+
+	const phone = String( request.body.phone ).replace(/[^\d]/g, ''); // Keep only digits
+
+	admin.auth().createUser( { uid : phone } )
+		.then( user => response.send( user ) )
+		.catch( error => response.status( 422 ).send( { error } ))
 }

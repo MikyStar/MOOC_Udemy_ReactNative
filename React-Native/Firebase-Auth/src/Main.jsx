8b1ab4d422118feb0748ef6314fbb059aa2372ee
Mike AUBENAS <mike.aubenas@gmail.com>
2018-11-22 13:52:09 +0100

    Add basic form component for phone number

--- React-Native/Firebase-Auth/src/Main.jsx
+++ React-Native/Firebase-Auth/src/Main.jsx
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+
+import SignUpForm from './components/SignUpForm';
 
 class Main extends Component
 {
 	render()
 	{
 		return	(
-					<Text>HIIIIIIIIIII !</Text>
+					<View style={ styles.container }>
+
+						<SignUpForm />
+
+					</View>
 				);
 	}
 }
+
+const styles = StyleSheet.create(
+{
+	container :
+	{
+		flex : 1,
+		justifyContent : 'center'
+	}
+});
 
 
 export default Main;

fb5b2e380fc41c5e19ef22f8c87b9e7f3ab38c16
Mike AUBENAS <mike.aubenas@gmail.com>
2018-11-23 16:24:43 +0100

    Add title + text component as a button at the right of header in review

--- React-Native/Jobs/src/screens/ReviewScreen.jsx
+++ React-Native/Jobs/src/screens/ReviewScreen.jsx
@@ -3,6 +3,18 @@ import { View, Text } from 'react-native';
 
 class ReviewScreen extends Component
 {
+	/**
+	 * The Navigator that I created using the react-navigation library is going to use this attribute
+	 */
+	static navigationOptions =
+	{
+		headerTitle : 'Review Jobs',
+		headerRight :
+		(
+			<Text>Go right</Text>
+		)
+	}
+
 	render()
 	{
 		return	 (

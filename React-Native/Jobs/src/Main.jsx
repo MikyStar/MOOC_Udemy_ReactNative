bc9d25515cff979e3628c5f4a3bffc8b0586af3f
Mike AUBENAS <mike.aubenas@gmail.com>
2018-11-23 16:08:02 +0100

    Finished creating 'the tree' of navigation

--- React-Native/Jobs/src/Main.jsx
+++ React-Native/Jobs/src/Main.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
-import { createAppContainer, createBottomTabNavigator } from "react-navigation";
+import { createAppContainer, createBottomTabNavigator, createStackNavigator } from "react-navigation";
 
 import AuthScreen from './screens/AuthScreen';
 import WelcomeScreen from './screens/WelcomeScreen';
 import MapScreen from './screens/MapScreen';
 import DeckScreen from './screens/DeckScreen';
+import ReviewScreen from './screens/ReviewScreen';
+import SettingsScreen from './screens/SettingsScreen';
 
 class Main extends Component
 {
@@ -20,7 +22,15 @@ class Main extends Component
 				screen : createBottomTabNavigator(
 				{
 					map : { screen : MapScreen },
-					deck : { screen : DeckScreen }
+					deck : { screen : DeckScreen },
+					review :
+					{
+						screen : createStackNavigator(
+						{
+							review : { screen : ReviewScreen },
+							settings : { screen : SettingsScreen }
+						})
+					}
 				})
 			}
 		});

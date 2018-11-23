92ec3349e82aa28f6d501bf927572648e0bcd5cf
Mike AUBENAS <mike.aubenas@gmail.com>
2018-11-23 16:35:16 +0100

    Now the button can make me navigate to SettingsScreen

--- React-Native/Jobs/src/screens/ReviewScreen.jsx
+++ React-Native/Jobs/src/screens/ReviewScreen.jsx
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { Button } from 'react-native-elements';
 
 class ReviewScreen extends Component
 {
 	/**
 	 * The Navigator that I created using the react-navigation library is going to use this attribute
 	 */
-	static navigationOptions =
-	{
+	static navigationOptions = ( { navigation } ) =>
+	({
 		headerTitle : 'Review Jobs',
 		headerRight :
 		(
-			<Text>Go right</Text>
+			<Button
+				title='Settings'
+				onPress={ () => { navigation.navigate( 'settings' ) } }
+			/>
 		)
-	}
+	});
 
 	render()
 	{

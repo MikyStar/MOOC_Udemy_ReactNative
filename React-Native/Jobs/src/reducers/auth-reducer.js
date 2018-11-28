1f7b92463404520b93cc0b3f0929bb04deeec9b5
Mike AUBENAS <mike.aubenas@gmail.com>
2018-11-28 16:22:11 +0100

    Properly kick user to map screen if already logged to FB

---
+++ React-Native/Jobs/src/reducers/auth-reducer.js
@@ -0,0 +1,16 @@
+import types from '../actions/types'
+
+export default function( state = {}, action )
+{
+	switch( action.type )
+	{
+		case types.FACEBOOK_LOGIN_SUCCESS :
+			return { token : action.payload };
+
+		case types.FACEBOOK_LOGIN_FAIL :
+			return { token : null }
+
+		default :
+			return state;
+	}
+}
--- React-Native/Jobs/src/screens/AuthScreen.jsx
+++ React-Native/Jobs/src/screens/AuthScreen.jsx
@@ -1,27 +1,40 @@
 import React, { Component } from 'react';
-import { View, Text, /* AsyncStorage */ } from 'react-native';
+import { View, Text } from 'react-native';
 import { connect } from 'react-redux';
 
 import * as actions from '../actions';
+import routes from './routes'
 
 class AuthScreen extends Component
 {
 	componentDidMount()
 	{
 		this.props.facebookLogin();
-		/* AsyncStorage.removeItem( 'fb_token' ); */
+		this.onAuthComplete( this.props );
+	}
+
+	onAuthComplete( props )
+	{
+		if( props.token )
+			this.props.navigation.navigate( routes.map );
+	}
+
+	componentWillReceiveProps( nextProps )
+	{
+		this.onAuthComplete( nextProps );
 	}
 
 	render()
 	{
 		return	(
-					<View>
-
-						<Text>AuthScreen</Text>
-
-					</View>
+					<View />
 				);
 	}
 }
 
-export default connect( null, actions )( AuthScreen );
+const mapStateToProps = ( { auth } ) =>
+{
+	return { token : auth.token };
+};
+
+export default connect( mapStateToProps, actions )( AuthScreen );
